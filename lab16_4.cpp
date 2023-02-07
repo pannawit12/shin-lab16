60fcb4113bc13b8a7ea28bf60e3cd7867427ebb2
Your Name <you@example.com>
2023-02-07 15:50:59 +0700

    finished lab16.4

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,35 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int r=rand()%4,*t=0,*t1=0;
+	if (r==0) {
+		t=&a;
+	}
+	else if (r==1) {
+		t=&b;
+	}
+	else if (r==2) {
+		t=&c;
+	}
+	else if (r==3) {
+		t=&d;
+	}
+	r=rand()%4;
+	if (r==0) {
+		t1=&a;
+	}
+	else if (r==1) {
+		t1=&b;
+	}
+	else if (r==2) {
+		t1=&c;
+	}
+	else if (r==3) {
+		t1=&d;
+	}
+	int t2=*t1;
+	*t1=*t;
+	*t=t2;
+}
